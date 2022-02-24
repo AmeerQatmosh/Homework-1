703c328508ac844447feeb35d0cfb1fb6f8fa62f
AmeerQatmosh <68341128+AmeerQatmosh@users.noreply.github.com>
2022-02-24 20:34:27 +0200

    implement webview

---
+++ Web View Application/Web View Application/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace Web_View_Application
+{
+    public partial class MainPage : ContentPage
+    {
+      
+        public MainPage()
+        {
+            InitializeComponent();
+            initialize();
+        }
+
+        void initialize()
+        {
+            date.Text = DateTime.Now.ToString();
+        }
+        void OnEntryCompleted(object sender, EventArgs args)
+        {
+            webView.Source = ((Entry)sender).Text;
+        }
+        void OnGoBackClicked(object sender, EventArgs args)
+        {
+            webView.GoBack();
+        }
+        void OnGoForwardClicked(object sender, EventArgs args)
+        {
+            webView.GoForward();
+        }
+
+        void onZajelButtonClicked(object sender, EventArgs args)
+        {
+            webView.Source = "https://zajel.najah.edu/";
+        }
+
+        void onGoogleButtonClicked(object sender, EventArgs args)
+        {
+            webView.Source = "https://google.com";
+        }
+
+ 
+    }
+}
+    
